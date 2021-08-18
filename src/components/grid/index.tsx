2e53477916bd83f244f4c8bc564351cf47949e7e
fatihk29 <fatihkrc7@gmail.com>
2021-08-19 00:21:31 +0300

    7th

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,1 +1,26 @@
 import React, { FC } from 'react';
+
+import Block from './block';
+import { Container, Row } from './styles';
+
+const Grid: FC = () => {
+  return (
+    <Container data-cy="grid-container">
+      {React.Children.toArray(
+        [...Array(9)].map((_, rowIndex) => (
+          <Row data-cy="grid-row-container" key={rowIndex}>
+            {React.Children.toArray(
+              [...Array(9)].map((_, colIndex) => (
+                <Block colIndex={colIndex} rowIndex={rowIndex} data-cy="block">
+                  {colIndex}
+                </Block>
+              ))
+            )}
+          </Row>
+        ))
+      )}
+    </Container>
+  );
+};
+
+export default Grid;
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,16 @@ import { ThemeProvider } from 'styled-components';
 import reportWebVitals from './reportWebVitals';
 
 import { GlobalStyles, theme } from './styles/';
-import { Card, Content, Title } from './components';
+import { Card, Content, Title, Grid } from './components';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
     <Content data-cy="content">
-      <Title data-cy="title">Hello</Title>
-      <Card data-cy="card">This is work in progress</Card>
+      <Title data-cy="title">Sudoku</Title>
+      <Card data-cy="card">
+        <Grid />
+      </Card>
     </Content>
   </ThemeProvider>,
   document.getElementById('root')
