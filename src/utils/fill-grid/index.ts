5a1e3accefe589d6c66108a4b3310ad5a9c0d45e
fatihk29 <fatihkrc7@gmail.com>
2021-08-24 11:42:55 +0300

    updated

--- src/utils/fill-grid/index.ts
+++ src/utils/fill-grid/index.ts
@@ -1,5 +1,5 @@
 import { GRID, NUMBERS } from 'typings';
-import { shuffle } from 'utils';
+import { isInRow, isInCol, shuffle } from 'utils';
 
 const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -17,9 +17,29 @@ function fillGrid(grid: GRID) {
 
     if (grid[row][col] === 0) {
       shuffle(numbers);
+
+      for (let value of numbers) {
+        if (!isInRow({ grid, row, value }))
+          if (!isInCol({ grid, col, value })) {
+            const square = [
+              [0, 0, 0],
+              [0, 0, 0],
+              [0, 0, 0],
+            ];
+            // is it not in the grid square?
+            //........ if this is the case
+
+            //...
+            grid[row][col] = value;
+            // check grid if it is full, if yes, stop and return true
+            // otherwise we run fillGrid(grid )
+          }
+      }
+
       break;
     }
   }
+
   grid[row][col] = 0;
 }
 
